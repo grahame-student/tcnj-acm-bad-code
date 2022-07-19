eb7a625e1a5b8b061515567b6ff09626078bdcff
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-07-19 15:17:33 +0100

    Update test_better_code.py

--- bad_code/tests/test_better_code.py
+++ bad_code/tests/test_better_code.py
@@ -5,5 +5,7 @@ from bad_code.bad import BetterCode
 
 
 class TestBetterCode(TestCase):
-    def test_(self):
-        assert_that(True, equal_to(True))
+    def test_get_random_alphabet_returns_26_chars(self):
+        better = BetterCode()
+        result = better.get_random_alphabet()
+        assert_that(len(result), equal_to(26))

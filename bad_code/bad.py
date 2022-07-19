f1d4bfdf9945d4d2be25543e08c82956d846c5f9
Grahame White <4whitg16@solent.ac.uk>
2022-07-19 15:27:03 +0100

    Applied formatting

--- bad_code/bad.py
+++ bad_code/bad.py
@@ -3,16 +3,38 @@ from random import shuffle as smoosh
 
 class BetterCode:
     def get_random_alphabet(self):
-        a = (self.the_stuff() | self.oh_my( 1,  2, 0) | self.oh_my(-2, 2, -2))
-        wer = a | set([55 // 11]) | {self.howdy('sick dudesfsd'), self.howdy('a') ** 2, self.howdy('10234'), self.howdy('m')}
+        a = self.the_stuff() | self.oh_my(1, 2, 0) | self.oh_my(-2, 2, -2)
+        wer = (
+            a
+            | set([55 // 11])
+            | {
+                self.howdy("sick dudesfsd"),
+                self.howdy("a") ** 2,
+                self.howdy("10234"),
+                self.howdy("m"),
+            }
+        )
 
-        x = {g % ((self.howdy('4') + 3) * 2) for g in (wer | {self.howdy('4') + 5} | {self.howdy('4') * 2 - 3} | {19, 21} | self.probably_okay() | {13} | self.oh_my(1, 2, 10) | self.oh_my(-2, 5, -2) | {11, 2, 4, 6, 8, 9})}
+        x = {
+            g % ((self.howdy("4") + 3) * 2)
+            for g in (
+                wer
+                | {self.howdy("4") + 5}
+                | {self.howdy("4") * 2 - 3}
+                | {19, 21}
+                | self.probably_okay()
+                | {13}
+                | self.oh_my(1, 2, 10)
+                | self.oh_my(-2, 5, -2)
+                | {11, 2, 4, 6, 8, 9}
+            )
+        }
 
         j = list(x)
         smoosh(j)
 
-        return ''.join(chr(ord(chr(97)) + j[i]) for i in j)
-        
+        return "".join(chr(ord(chr(97)) + j[i]) for i in j)
+
     def the_stuff(self):
         a = 10
         y = []
@@ -29,13 +51,23 @@ class BetterCode:
             the_other_stuff.add(int(g / ((1 + 4 + 2 - 3 + 1 + 5) / 5)))
             the_other_stuff.add(g)
 
-        return  (the_other_stuff | {-a}) | {was // 2 for was in (the_other_stuff | {-a})}
+        return (the_other_stuff | {-a}) | {was // 2 for was in (the_other_stuff | {-a})}
 
     def oh_my(self, b, a, c):
-        return {((cricket ** 2) // 5) for cricket in {b + a + c * 2, b + a // 3 + c * 2, 10 // b + a // 3 + c * 2} if True or False}
+        return {
+            ((cricket**2) // 5)
+            for cricket in {b + a + c * 2, b + a // 3 + c * 2, 10 // b + a // 3 + c * 2}
+            if True or False
+        }
 
     def howdy(self, a):
         return int(sum(ord(c) for c in a)) % 100 // 5
-    
+
     def probably_okay(self):
-        return set(map(lambda x: int(abs(self.howdy(str(x))) ** (1 / 2)) % (self.howdy('m') * 2), self.the_stuff()))
+        return set(
+            map(
+                lambda x: int(abs(self.howdy(str(x))) ** (1 / 2))
+                % (self.howdy("m") * 2),
+                self.the_stuff(),
+            )
+        )

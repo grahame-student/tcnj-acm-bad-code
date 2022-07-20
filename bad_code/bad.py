a28302770745f6902bc25f564e54f7fa45701c1e
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-07-20 23:10:07 +0100

    Update bad.py

--- bad_code/bad.py
+++ bad_code/bad.py
@@ -1,29 +1,11 @@
-from random import shuffle as smoosh
+from random import shuffle
 
 
 class BetterCode:
-    def get_random_alphabet(self):
-        wer = (
-            {0, -12, -1, -8, -6, -4, -3, -2}
-            | {0, 1, 20}
-            | {16, 3, 7}
-            | {5}
-            | {1, 361, 10, 1}
-            | {15}
-            | {17}
-            | {19, 21}
-            | {0, 1}
-            | {13}
-            | {88, 105, 180}
-            | {0, 12, 5}
-            | {11, 2, 4, 6, 8, 9}
-        )
-
-        x = {g % 26 for g in (wer)}
-
+    def get_random_alphabet(self):        
+        x = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}
         j = list(x)
-        smoosh(j)
-
+        shuffle(j)
         return "".join(chr(ord(chr(97)) + j[i]) for i in j)
 
     def the_stuff(self):

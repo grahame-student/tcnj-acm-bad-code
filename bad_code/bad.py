f0ca66d444b85fcd95e33da151d6c15ec0e9330a
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-07-20 21:28:28 +0100

    Simplified maths operations

--- bad_code/bad.py
+++ bad_code/bad.py
@@ -6,21 +6,21 @@ class BetterCode:
         a = self.the_stuff() | self.oh_my(1, 2, 0) | self.oh_my(-2, 2, -2)
         wer = (
             a
-            | set([55 // 11])
+            | set([5])
             | {
                 1,
-                19**2,
+                361,
                 10,
                 1,
             }
         )
 
         x = {
-            g % ((10 + 3) * 2)
+            g % 26
             for g in (
                 wer
-                | {10 + 5}
-                | {10 * 2 - 3}
+                | {15}
+                | {17}
                 | {19, 21}
                 | self.probably_okay()
                 | {13}
@@ -44,11 +44,11 @@ class BetterCode:
         while a > 3:
             a = a - 2
             y = y + [m - a * 2]
-            t = 2 + t + 1
+            t = t + 3
 
         the_other_stuff = set([18 - t for x in [1, 2, 10]])
         for g in y:
-            the_other_stuff.add(int(g / ((1 + 4 + 2 - 3 + 1 + 5) / 5)))
+            the_other_stuff.add(int(g / 2))
             the_other_stuff.add(g)
 
         return (the_other_stuff | {-a}) | {was // 2 for was in (the_other_stuff | {-a})}
@@ -66,7 +66,7 @@ class BetterCode:
     def probably_okay(self):
         return set(
             map(
-                lambda x: int(abs(self.howdy(str(x))) ** (1 / 2)) % (1 * 2),
+                lambda x: int(abs(self.howdy(str(x))) ** (1 / 2)) % 2,
                 self.the_stuff(),
             )
         )

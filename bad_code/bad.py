d9789aa94bb0246de142726ca787896080d3a810
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-07-21 20:10:02 +0100

    Added deprecated decorators to the unused methods

--- bad_code/bad.py
+++ bad_code/bad.py
@@ -1,4 +1,4 @@
-from deprecation import deprecated
+from deprecated import deprecated
 from random import shuffle
 
 
@@ -8,17 +8,22 @@ class BetterCode:
         shuffle(j)
         return "".join(chr(ord("a") + j[i]) for i in j)
 
+    @deprecated(reason="This method is deprecated and will be removed in a future version")
     def the_stuff(self):
         return {0, -12, -1, -8, -6, -4, -3, -2}
 
+    @deprecated(reason="This method is deprecated and will be removed in a future version")
     def oh_my(self, b, a, c):
+        values = {b + a + c * 2, b + a // 3 + c * 2, 10 // b + a // 3 + c * 2}
         return {
-            ((cricket**2) // 5)
-            for cricket in {b + a + c * 2, b + a // 3 + c * 2, 10 // b + a // 3 + c * 2}
+            ((value**2) // 5)
+            for value in values
         }
 
+    @deprecated(reason="This method is deprecated and will be removed in a future version")
     def howdy(self, a):
         return int(sum(ord(c) for c in a)) % 100 // 5
 
+    @deprecated(reason="This method is deprecated and will be removed in a future version")
     def probably_okay(self):
         return {0, 1}

d7352602fd3672a01a60e9def90371239085d0fa
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-07-21 20:26:14 +0100

    Update bad.py

--- bad_code/bad.py
+++ bad_code/bad.py
@@ -1,12 +1,13 @@
-from deprecated import deprecated
 from random import shuffle
+
+from deprecated import deprecated
 
 
 class BetterCode:
     def get_random_alphabet(self):
         """
         Generate a random shuffle of the lower case letters a-z
-        
+
         :return: Returns a string containing the shuffled lower case letters a-z
         :rtype:  string
         """
@@ -14,11 +15,15 @@ class BetterCode:
         shuffle(j)
         return "".join(chr(ord("a") + j[i]) for i in j)
 
-    @deprecated(reason="This method is deprecated and will be removed in a future version")
+    @deprecated(
+        reason="This method is deprecated and will be removed in a future version"
+    )
     def the_stuff(self):
         return {0, -12, -1, -8, -6, -4, -3, -2}
 
-    @deprecated(reason="This method is deprecated and will be removed in a future version")
+    @deprecated(
+        reason="This method is deprecated and will be removed in a future version"
+    )
     def oh_my(self, b, a, c):
         values = {b + a + c * 2, b + a // 3 + c * 2, 10 // b + a // 3 + c * 2}
         return {
@@ -26,15 +31,19 @@ class BetterCode:
             for value in values
         }
 
-    @deprecated(reason="This method is deprecated and will be removed in a future version")
+    @deprecated(
+        reason="This method is deprecated and will be removed in a future version"
+    )
     def howdy(self, a):
         return int(sum(ord(c) for c in a)) % 100 // 5
 
-    @deprecated(reason="This method is deprecated and will be removed in a future version")
+    @deprecated(
+        reason="This method is deprecated and will be removed in a future version"
+    )
     def probably_okay(self):
         """
         Generate a set containing the numers 0 and 1
-        
+
         :return: Returns a set instance containing the integers 0 and 1
         :rtype:  set
         """
